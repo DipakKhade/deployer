72401561473db19130cdcd2097bd3ed65d1d7f77
= <=>
2024-08-10 21:40:28 +0530

    pushing process id to redis queue

--- uploader/src/routes/uploadToS3.ts
+++ uploader/src/routes/uploadToS3.ts
@@ -3,18 +3,39 @@ import { Clone } from "../lib/clone";
 import { create_id } from "../lib/clone";
 import { getAllFilesAndDirectories } from "../lib/files";
 import { uploadFilesToS3 } from "../lib/s3";
+import { createClient , RedisClientType} from 'redis'
+
+
 export const uploadToS3Router = Router();
+
+let Publisher : RedisClientType;
+(async()=>{
+   Publisher = createClient({
+     url: 'redis://localhost:6379'
+  }) 
+  await Publisher.connect()
+})();
 
 uploadToS3Router.post("/gitrepo", async (req, res) => {
   const { gitrepourl } = req.body;
+
   const id = create_id();
+
   try {
     const c = await Clone(gitrepourl, id);
+
     const files_to_upload = getAllFilesAndDirectories(`D:\\Dipak\\uploader\\uploads\\${id}`);
       
     await uploadFilesToS3(files_to_upload);
 
-  } catch (e) {}
+    await Publisher.lPush('repo-queue', id);
+
+  } catch (e) {
+
+    console.log(e)
+
+  }
+
   res.json({
     id,
   });
