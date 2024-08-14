c9ac58d1a252b5594f9516679b3eeb3992ff474c
= <=>
2024-08-14 23:50:46 +0530

    hero component added

---
+++ frontend/src/components/AppBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Appbar() {
+  return (
+    <>
+      <nav className="flex justify-between h-12 w-full border border-slate-300 p-2">
+        <div>Deployer</div>
+        <div>sign in</div>
+      </nav>
+    </>
+  );
+}
